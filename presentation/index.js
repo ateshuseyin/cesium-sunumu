f4738371cb8654b81ef703938a1bda69d303ad6e
hates <ates.hueseyin@gmail.com>
2018-04-19 00:56:18 +0300

    herşeyin başı sağlık

--- presentation/index.js
+++ presentation/index.js
@@ -5,8 +5,10 @@ import React from "react";
 import {
   BlockQuote,
   Cite,
+  CodePane,
   Deck,
   Heading,
+  Image,
   ListItem,
   List,
   Quote,
@@ -14,11 +16,19 @@ import {
   Text
 } from "spectacle";
 
+
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
 require("normalize.css");
+
+const images = {
+  cesiumLogo: require('../assets/cesium_color_black.png'),
+  myself: require('../assets/myself.jpg')
+};
+
+const helloWorld = require('../assets/helloworld.txt');
 
 const theme = createTheme({
   primary: "white",
@@ -30,41 +40,48 @@ const theme = createTheme({
   secondary: "Helvetica"
 });
 
+const fullPage = {
+  position: 'absolute',
+  top: 0,
+  bottom: 0,
+  left: 0,
+  right: 0
+};
+
 export default class Presentation extends React.Component {
   render() {
     return (
       <Deck transition={["zoom", "slide"]} transitionDuration={500} theme={theme}>
         <Slide transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
+          <Image src={images.cesiumLogo}/>
+          <Text textColor="tertiary" size={1} bold>
+            Javascript Ankara
+          </Text>
+          <Text textColor="tertiary" size={1} bold>
+            Nisan 2018
           </Text>
         </Slide>
-        <Slide transition={["fade"]} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>Typography</Heading>
-          <Heading size={1} textColor="secondary">Heading 1</Heading>
-          <Heading size={2} textColor="secondary">Heading 2</Heading>
-          <Heading size={3} textColor="secondary">Heading 3</Heading>
-          <Heading size={4} textColor="secondary">Heading 4</Heading>
-          <Heading size={5} textColor="secondary">Heading 5</Heading>
-          <Text size={6} textColor="secondary">Standard text</Text>
+        <Slide transition={["fade"]} bgColor="tertiary" bgImage={images.myself}>
         </Slide>
         <Slide transition={["fade"]} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>Standard List</Heading>
+          <Heading size={6} textColor="secondary" caps>An open-source JavaScript library for world-class 3D globes and
+            maps</Heading>
           <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
+            <ListItem>3045 Github Yıldızı</ListItem>
+            <ListItem>1346 Fork</ListItem>
+            <ListItem>10 Geliştirici</ListItem>
+            <ListItem>150 Katkı Sağlayıcı</ListItem>
+            <ListItem>143K Kaynak Kod + 131K Test Kodu</ListItem>
+            <ListItem>%95 Coverage</ListItem>
+            <ListItem>Mükemmel Dokümantasyon</ListItem>
           </List>
         </Slide>
         <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
+          <iframe src="https://cesiumjs.org/Cesium/Build/Apps/CesiumViewer" className="fill">
+          </iframe>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="secondary" textColor="primary">
+          <CodePane source={helloWorld} lang="html"/>
         </Slide>
       </Deck>
     );
